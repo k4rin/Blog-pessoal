4a16e84d978c80f4d32c03c00eb210fcc497eb3d
K4rin <karinecris.mix@gmail.com>
2025-09-24 15:50:42 -0300

    Implementando as classes de Tema

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { TemaModule } from "src/tema/tema.module";
 import { Postagem } from "./entities/postagem.entity";
 import { PostagemController } from "./controllers/postagem.controller";
 import { PostagemService } from "./services/postagem.service";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Postagem])
+        TypeOrmModule.forFeature([Postagem]), TemaModule
     ],
     providers: [PostagemService],
     controllers: [PostagemController], 
